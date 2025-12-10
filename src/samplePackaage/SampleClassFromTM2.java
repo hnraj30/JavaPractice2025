1ce184170f6308a74506d78e3ebf4a1efa0ace44
hnraj <hnraj@Raj-HP245G8>
2025-12-10 16:32:19 +0530

    cimmitting to test pull from remote repo

--- src/samplePackaage/SampleClassFromTM2.java
+++ src/samplePackaage/SampleClassFromTM2.java
@@ -6,6 +6,9 @@ public class SampleClassFromTM2 {
 		System.out.println("Statement added from Team member 2");
 		System.out.println("Adding this statement to test pull request");
 		System.out.println("Adding statement to test Fetch");
+		
+		System.out.println("Adding statement to test pull");
+
 
 	}
 
